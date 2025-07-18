e753aba6c0cfc62530bd2ffa24077c269dcfd977
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-07-18 14:26:39 -0400

    Add files via upload

--- R/dropletqc.R
+++ R/dropletqc.R
@@ -4,7 +4,9 @@
 #' @param dir cellranger out dir
 #' @export
 #'
-RunDropletQC <- function(dir=NULL, form='cellranger'){
+RunDropletQC <- function(dir=NULL, form='cellranger', outdir=''){
+	dnf <- NULL
+
 	if (form == 'cellranger'){
 		dnf <- DropletQC::nuclear_fraction_tags(outs = dir, tiles = 1, cores = 1, verbose = FALSE)
 	} 
@@ -15,7 +17,15 @@ RunDropletQC <- function(dir=NULL, form='cellranger'){
 		dnf <- DropletQC::nuclear_fraction_tags(bam = bam, barcodes=barcodes, tiles = 1, cores = 1, verbose = FALSE)
 	}
     
-	write.table(dnf, file=paste0(dir, '/nuclear_fraction.tsv'), sep='\t', quote=FALSE, col.names=FALSE)
+
+	if (nchar(outdir) == 0){
+		outdir <- dir
+	} else {
+		dir.create(outdir)
+	}
+	
+	write.table(dnf, file=paste0(outdir, '/nuclear_fraction.tsv'), sep='\t', quote=FALSE, col.names=FALSE)
+	
 }
 
 
@@ -23,25 +33,28 @@ RunDropletQC <- function(dir=NULL, form='cellranger'){
 
 #' DropletQC empty drops
 #'
-#' @param fnf dropletqc data
-#' @param fmeta metadata
+#' @param fnf dropletqc data file
+#' @param meta metadata data-frame
 #' @param outdir out dir
 #' @export
 #'
-RunEmptyDrop <- function(
-	nf=NULL, meta=NULL, 
+EstimateEmptyDrops <- function(
+	fnf=NULL, meta=NULL, 
 	nf_rescue=0.05, 
 	umi_rescue=1000, 
 	include_plot=FALSE,
 	col_ct='', 
 	outdir='.'
 ){
-	droplet <- read.table(nf, sep='\t', header=F)
-	metadata <- read.table(meta, sep='\t', header=T, row.names=1)
 	dir.create(outdir)
 
-	metadata$nf <- droplet$V2[match(rownames(metadata), droplet$V1)]
-	nf_umi <- metadata[, c('nf', 'nCount_RNA')]
+	dnf <- read.table(fnf, sep='\t', header=F)
+	#meta <- read.table(fmeta, sep='\t', header=T, row.names=1)
+
+	colnames(dnf) <- c('cell', 'nf')
+
+	meta$nf <- dnf$nf[match(rownames(meta), dnf$cell)]
+	nf_umi <- meta[, c('nf', 'nCount_RNA')]
 	colnames(nf_umi) <- c('nf', 'umi_count')
 	write.table(nf_umi, file=paste0(outdir,'/nf_umi.tsv'), sep='\t', quote=FALSE, col.names=NA)
 
@@ -59,7 +72,7 @@ RunEmptyDrop <- function(
 
 	# Identify damaged cells
 	if (nchar(col_ct) > 0){
-		nfc.ed$cell_type <- metadata[[col_ct]]
+		nfc.ed$cell_type <- meta[[col_ct]]
 
 		nfc.ed_dc <- DropletQC::identify_damaged_cells(nfc.ed, verbose = FALSE)
 		write.table(nfc.ed_dc, file=paste0(outdir,'/nf_umi.damaged_cells.tsv'), sep='\t', quote=FALSE, col.names=NA)
@@ -72,28 +85,22 @@ RunEmptyDrop <- function(
 
 
 
-#' DropletQC - define and remove empty-drop
+#' Filter empty droplet using DropletQC 
 #'
 #' @param dir cellranger out dir
 #' @param obj seurat object
 #' @export
 #'
-RunDelEmptyDrop <- function(dir=NULL, obj=NULL, outdir='.'){
-    dnf <- DropletQC::nuclear_fraction_tags(outs = dir, tiles = 1, cores = 1, verbose = FALSE)
-    #   nuclear_fraction
-	write.table(dnf, file=paste0(dir, '/nuclear_fraction.tsv'), sep='\t', quote=FALSE, col.names=FALSE)
-
+RunFilterEmptyDrops <- function(dir=NULL, obj=NULL, form='cellranger', outdir='.'){
 	meta <- obj@meta.data
 
-	meta$nf <- dnf$nuclear_fraction[match(rownames(meta), rownames(dnf))]
-	nf_umi <- meta[, c('nf', 'nCount_RNA')]
-	colnames(nf_umi) <- c('nf', 'umi_count')
+	RunDropletQC(dir=dir, form=form, outdir=outdir, set_write=TRUE, set_return=TRUE)
+	EstimateEmptyDrops(fnf=paste0(outdir, '/nuclear_fraction.tsv'), meta=meta, outdir=outdir)
 
-	nf_ed <- DropletQC::identify_empty_drops(nf_umi=nf_umi)
-	write.table(nf_ed, file=paste0(outdir,'/nf_umi.empty_drops.tsv'), sep='\t', quote=FALSE, col.names=NA)
+	empty_drop <- read.table(paste0(outdir, '/nf_umi.empty_drops.tsv'), sep='\t', header=T, row.names=1)
 
 	# real cell (filtered emptydrop)
-	real_cell <- rownames(nf_ed)[nf_ed$cell_status == 'cell']
+	real_cell <- rownames(empty_drop)[empty_drop$cell_status == 'cell']
 	obj <- subset(obj, cells=real_cell)
 
 	return(obj)
