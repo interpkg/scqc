4b9a29573d9fbb9d53054f9560f17435024529eb
Hua Sun <33794673+hsun9@users.noreply.github.com>
2026-02-02 15:33:31 -0500

    Add files via upload

--- R/soupx.R
+++ R/soupx.R
@@ -5,18 +5,24 @@
 #' @export
 #'
 RunSoupX <- function(dir=NULL, adj=FALSE){
+    out_dir <- paste0(dir, '/soupx')
+    dir.create(out_dir)
+
+    # read
     sc <- SoupX::load10X(dir)
-    pdf(paste0(dir, '/soupx.raw.pdf'), w=6, h=6)
+
+    # out
+    pdf(paste0(out_dir, '/soupx.raw.pdf'), w=6, h=6)
     sc <- SoupX::autoEstCont(sc)
     dev.off()
 
     # soupx- Estimated global rho
-    writeLines(unique(sc$metaData$rho), paste0(dir, '/soupx.raw.global_rho.out'))
+    write(unique(sc$metaData$rho), file=paste0(out_dir, '/soupx.raw.global_rho.out'))
 
     # adjust counts
     if (adj){
         adj.matrix <- SoupX::adjustCounts(sc, roundToInt = TRUE)
-        DropletUtils:::write10xCounts(paste0(dir,'/soupx_adjustCounts.h5'), adj.matrix, type="HDF5", overwrite=TRUE)
+        DropletUtils:::write10xCounts(paste0(out_dir,'/soupx_adjustCounts.h5'), adj.matrix, type="HDF5", overwrite=TRUE)
     }
 }
 
